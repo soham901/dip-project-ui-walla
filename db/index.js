da0a7a83d13d57596a3319232715dfd0a0bf02bc
soham <sagathiyasoham12345@gmail.com>
2023-12-11 13:26:54 +0530

    connected mongo atlas

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/ui-components");
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 
