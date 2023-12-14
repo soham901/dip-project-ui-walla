50069c0dc1a44951a1cba419616f78c41e66e7bd
soham <sagathiyasoham12345@gmail.com>
2023-12-14 10:40:22 +0530

    DONE

--- index.js
+++ index.js
@@ -93,7 +93,32 @@ app.get("/component", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    const html = fs.readFileSync("public/site.html", "utf-8");
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <form action="https://ui-comp.vercel.app/component" method="post">
+          <label for="title">title</label>
+          <input type="text" name="title" id="title" />
+    
+          <label for="html">html</label>
+          <textarea name="html" id="html" cols="30" rows="10"></textarea>
+    
+          <label for="css">css</label>
+          <textarea name="css" id="css" cols="30" rows="10"></textarea>
+    
+          <label for="js">js</label>
+          <textarea name="js" id="js" cols="30" rows="10"></textarea>
+    
+          <input type="submit" value="submit" />
+        </form>
+      </body>
+    </html>
+    `
     res.send(html);
 });
 
