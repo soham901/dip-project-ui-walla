3ec1cce0892a1e14cce3a33245cb83d512fc1884
soham <sagathiyasoham12345@gmail.com>
2023-12-20 11:21:06 +0530

    clean

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
-import fs from "fs";
-import mongoose from "mongoose";
 
 dotenv.config({ path: '.env' });
 
@@ -21,48 +19,16 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-import { connectDB } from "./db/index.js";
+import { connectDB } from "./db.js";
 
 connectDB();
 
 import { Comp } from "./models.js";
 
-// const Comp = mongoose.model("Comp", mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: true,
-//     },
-//     code: {
-//         type: String,
-//     },
-// }));
-
-// import componentRouter from "./routes/component.js";
-
-// app.use("/component", componentRouter);
-
 app.post("/component", (req, res) => {
     console.log(req.body);
     const { html, css, js, title } = req.body;
     const category = req?.body?.category || "general";
-    // const code = `
-    // <!DOCTYPE html>
-    // <html lang="en">
-    // <head>
-    //     <meta charset="UTF-8">
-    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //     <title>${title}</title>
-    //     <style>${css}</style>
-    // </head>
-    // <body>
-    //     ${html}
-
-    //     <script>
-    //         ${js}
-    //     </script>
-    // </body>
-    // </html>
-    // `;
 
     Comp.create({ title: title, html: html, css: css, js: js, category: category }).then((data) => {
         console.log(data);
@@ -113,41 +79,6 @@ app.get("/component", (req, res) => {
     }
 });
 
-app.get("/test", (req, res) => {
-    const html = `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
-      <body>
-        <form action="https://ui-comp.vercel.app/component" method="post">
-          <label for="title">title</label>
-          <input type="text" name="title" id="title" />
-    
-          <label for="html">html</label>
-          <textarea name="html" id="html" cols="30" rows="10"></textarea>
-    
-          <label for="css">css</label>
-          <textarea name="css" id="css" cols="30" rows="10"></textarea>
-    
-          <label for="js">js</label>
-          <textarea name="js" id="js" cols="30" rows="10"></textarea>
-    
-          <input type="submit" value="submit" />
-        </form>
-      </body>
-    </html>
-    `
-    // res.send(html);
-    res.sendFile(__dirname + "/index.html")
-});
-
-app.get("/", (req, res) => {
-    res.send("ok")
-})
-
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log("BACKEND IS RUNNING");
 });
