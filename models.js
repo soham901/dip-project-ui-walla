62d565d2b9a1042770b89e3173bcd9bbc54245c1
soham <sagathiyasoham12345@gmail.com>
2023-12-20 10:09:59 +0530

    add models

---
+++ models.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+export const Comp = mongoose.model("Comp", mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    html: {
+        type: String,
+        required: true,
+    },
+    css: {
+        type: String,
+        required: true,
+    },
+    js: {
+        type: String,
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+}));
