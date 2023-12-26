c3aa75f1cff23cf9c4abcf7827f2af14afe8b53e
soham <sagathiyasoham12345@gmail.com>
2023-12-26 05:43:21 +0530

    config changes

---
+++ main.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is running");
+});
