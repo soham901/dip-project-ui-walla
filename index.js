53a734a42f0cf2e4ab8cda724231da47d61e6cb1
soham <sagathiyasoham12345@gmail.com>
2023-12-28 06:05:02 +0530

    update

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 import express from "express";
-
 import cors from "cors";
-import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+// import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
 
 dotenv.config({ path: '.env' });
-
-const app = express();
-
-app.use(cors({
-    origin: "*",
-    credentials: true
-}));
-
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.static("public"))
-app.use(cookieParser())
-
 
 import { connectDB } from "./db.js";
 
@@ -25,24 +12,46 @@ connectDB();
 
 import { Comp } from "./models.js";
 
-app.post("/component", (req, res) => {
-    console.log(req.body);
-    const { html, css, js, title } = req.body;
-    const category = req?.body?.category || "general";
+const app = express();
 
-    Comp.create({ title: title, html: html, css: css, js: js, category: category }).then((data) => {
-        console.log(data);
-        res.send("DONE");
-    })
+app.use(cors(
+    {
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: "GET, POST, PUT, DELETE",
+        allowedHeaders: "Content-Type, Authorization, Origin, X-Requested-With, Accept"
+    }
+));
+
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static("public"))
+// app.use(cookieParser())
+app.use(bodyParser.text())
+app.options('*', cors())
+
+
+app.get("/", (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
 
-app.get("/component", (req, res) => {
 
-    const id = req.query.id;
-    const preview = req.query.preview;
+app.post("/", (req, res) => {
+    console.log(req.body);
+    const { name } = req.body;
+    res.json({ message: `Hello ${name}` })
+});
+
+
+app.get("/component", (req, res) => {
+    const id = req.query?.id;
+    const preview = req.query?.preview;
 
     if (id) {
-        if (preview) {
+        if (preview == "true") {
+            console.log("preview");
             Comp.findById(id).then((data) => {
                 const code = `
                 <!DOCTYPE html>
@@ -65,6 +74,34 @@ app.get("/component", (req, res) => {
                 res.send(code)
             })
         }
+        else if (preview == "false") {
+            console.log("download");
+            Comp.findById(id).then((data) => {
+                const code = `
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>${data.title}</title>
+                    <style>${data.css}</style>
+                </head>
+                <body>
+                    ${data.html}
+
+                    <script>
+                        ${data.js}
+                    </script>
+                </body>
+                </html>
+                `;
+                res.setHeader('Content-disposition', 'attachment; filename=component.html');
+                res.setHeader('Content-type', 'text/html');
+                res.charset = 'UTF-8';
+                res.write(code);
+                res.end();
+            })
+        }
         else {
             Comp.findById(id).then((data) => {
                 res.send(data);
@@ -79,6 +116,19 @@ app.get("/component", (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("BACKEND IS RUNNING");
+
+app.post("/component", (req, res) => {
+    const body = typeof req.body == "string" ? JSON.parse(req.body) : req.body;
+    const { html, css, js, title } = body;
+    const category = body?.category || "general";
+
+    Comp.create({ title: title, html: html, css: css, js: js, category: category }).then((data) => {
+        console.log(data);
+        res.json({ message: "Component Saved" })
+    })
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running");
 });
